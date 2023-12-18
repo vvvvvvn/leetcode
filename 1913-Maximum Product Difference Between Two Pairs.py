d7c20cf4986b57c7dc9fb217e9263096663fd883
Paul <alphagoo@g-mail.nsysu.edu.tw>
2023-12-18 22:52:15 +0800

    feat: :sparkles: solve problem 1913

---
+++ 1913-Maximum Product Difference Between Two Pairs.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[-1] * nums[-2] - nums[0] * nums[1]
+
+
+if __name__ == "__main__":
+    nums = [4, 2, 5, 9, 7, 4, 8]
+    print(Solution().maxProductDifference(nums))
